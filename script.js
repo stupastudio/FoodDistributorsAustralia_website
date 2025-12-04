696ba552dd9bf0da32002c8e9b97b866a738845a
STUPASTUDIO <stupastudio.np@gmail.com>
2025-12-05 03:32:53 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function goTo(page) {
+    window.location.href = page;
+}
+
+const navSlide = () => {
+    const burger = document.querySelector('.hamburger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        // Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                // Adjusting animation delay based on index for cascade effect
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+
+        // Burger Animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+// Call the function immediately to attach listeners
+navSlide();
